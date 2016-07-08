8a5d4c3e6a3ceab6e5d3b06643201c778c52e594
Andreas Okholm <aokholm@gmail.com>
2016-07-08 15:32:07 +0200

    initial commit

--- KiteRemote/ViewController.swift
+++ KiteRemote/ViewController.swift
@@ -7,17 +7,61 @@
 //
 
 import UIKit
+import Starscream
 
 class ViewController: UIViewController {
+    
+    let socket = WebSocket(url: NSURL(string: "ws://192.168.4.1:81/")!)
 
+    var lastTime = NSDate()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //websocketDidReceiveMessage
+        socket.onText = { (text: String) in
+            print("got some text: \(text)")
+        }
+        
+        
+        socket.connect()
+        socket.onConnect = {
+            print("connected woho!")
+        }
+        socket.onDisconnect = { (err: NSError?) in
+            if let err = err {
+                print(err)
+            } else {
+                print("disconnected without error")
+            }
+            
+        }
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func newSliderValue(sender: UISlider) {
+        newPos( Int( (sender.value - 0.5) * 1600*10 ) )
+    }
+    
+    
+    func newPos(pos: Int) {
+        if (lastTime.timeIntervalSinceNow < -0.05) {
+            
+            if socket.isConnected {
+                socket.writeString(String(pos))
+            }
+            
+            lastTime = NSDate()
+        }
     }
 
 
